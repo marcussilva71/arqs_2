d4ac7a832ba1735625afc62df4eedf9d9da9fc0b
115245367 <115245367@ESTB8LI0623.animaedu.intranet>
2018-06-27 21:57:27 -0300

    Lab1 - Completo

--- src/test/java/loja/teste.java
+++ src/test/java/loja/teste.java
@@ -1,40 +1,32 @@
 package loja;
 
-import static org.junit.Assert.assertEquals;
-
-import java.math.BigDecimal;
 import java.util.Date;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import br.unibh.loja.entidades.Categoria;
 import br.unibh.loja.entidades.Cliente;
-import br.unibh.loja.entidades.Produto;
 
 
 public class teste {
 
 	@Test
-	public void testarCliente() {
+	public void PrintObject() {
 		Cliente c1 = new Cliente(1L, "nome", "login", "senha", "perfil", "cpf", "telefone", "email", new Date(), new Date());
-		assertEquals(c1.getCpf(), "cpf");
 		System.out.println(c1);
 	}
 	
 	@Test
-	public void testarCategoria() {
-		Categoria cat1 = new Categoria(1L, "descricao");
-		assertEquals(cat1.getDescricao(), "descricao");
-		System.out.println(cat1);
+	public void EqualsObject() {
+		Cliente c1 = new Cliente(1L, "nome", "login", "senha", "perfil", "cpf", "telefone", "email", new Date(), new Date());
+		Cliente c2 = new Cliente(1L, "nome1", "login1", "senha1", "perfil1", "cpf1", "telefone1", "email1", new Date(), new Date());
+		Assert.assertTrue(c1.equals(c2));
 	}
 	
 	@Test
-	public void testarProduto() {
-		Categoria cat1 = new Categoria(1L, "descricao");
-		Produto p1 = new Produto(1L,"nome", "descricao", cat1, new BigDecimal(10000), "fabricante");
-		assertEquals(p1.getFabricante(), "fabricante");
-		System.out.println(p1);
-		
+	public void ToStringObject() {
+		Cliente c1 = new Cliente(1L, "nome", "login", "senha", "perfil", "cpf", "telefone", "email", new Date(), new Date());
+		System.out.print(c1.toString());
 	}
 
 }
